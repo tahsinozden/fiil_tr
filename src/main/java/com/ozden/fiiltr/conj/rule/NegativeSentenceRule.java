8f7c3ca5ab2e463e1f012d453812d51a37c6fe19
tahsinozden <tahsinozden@gmail.com>
2018-04-04 22:11:53 +0200

    removed unnecessary code.

--- src/main/java/com/ozden/fiiltr/conj/rule/NegativeSentenceRule.java
+++ src/main/java/com/ozden/fiiltr/conj/rule/NegativeSentenceRule.java
@@ -1,8 +1,5 @@
 package com.ozden.fiiltr.conj.rule;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public enum NegativeSentenceRule implements Rule {
     NEGATIVE {
         @Override
@@ -10,20 +7,6 @@ public enum NegativeSentenceRule implements Rule {
             return ImperativeRule.NEGATIVE.apply(word);
         }
     };
-
-    private static final Map<Character, String> NEGATIVE_RULE_WORDS;
-
-    static {
-        NEGATIVE_RULE_WORDS = new HashMap<>();
-//        NEGATIVE_RULE_WORDS.put('a', "mı");
-//        NEGATIVE_RULE_WORDS.put('ı', "mı");
-//        NEGATIVE_RULE_WORDS.put('u', "mu");
-//        NEGATIVE_RULE_WORDS.put('o', "mu");
-//        NEGATIVE_RULE_WORDS.put('e', "mi");
-//        NEGATIVE_RULE_WORDS.put('i', "mi");
-//        NEGATIVE_RULE_WORDS.put('ü', "mü");
-//        NEGATIVE_RULE_WORDS.put('ö', "mü");
-    }
 
     @Override
     public int getOrder() {
